ebc8313184ad9cda21c4295046863f7f242e405d
phoenixdevcreations <phoenixdevcreations@gmail.com>
2025-12-26 12:39:01 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    base: '/phoenixdevcreations/',
+    base: '/FitPlan/',
     return {
       server: {
         port: 3000,
